6c7692227ef26656cd8d27f6f0773063198929c4
Andrew Ackerman <andrew.ackers@gmail.com>
2019-11-01 09:12:42 -0700

    header and tab complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,36 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        console.log(data)
+    })
+
+
+function CreateCard(arg) {
+    const articleCard = document.createElement("div"),
+        articleHeadline = document.createElement("div"),
+        articleAuthor = document.createElement("div"),
+        articleImgContainer = document.createElement("div"),
+        articleImg = document.createElement("img"),
+        articleCredit = document.createElement("span");
+
+    articleCard.appendChild(articleHeadline);
+    articleCard.appendChild(articleAuthor);
+    articleAuthor.appendChild(articleImgContainer);
+    articleImgContainer.appendChild(articleImg);
+    articleAuthor.appendChild(articleCredit);
+
+    articleHeadline.textContent = arg. //TODO ;
+    articleAuthor.textContent = arg. //TODO ;
+    articleImg.src = arg. //TODO ;
+    articleCredit.textContent = `By ${arg.TODO}`;
+
+    articleCard.classList.add("card");
+    articleHeadline.classList.add("headline");
+    articleAuthor.classList.add("author");
+    articleImgContainer.classList.add("img-container")
+
+    return CreateCard;
+}
